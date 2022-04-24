29b6f6ff0121530b1155bf4547d0ba5839f3bda1
Jonas Deipenbrock <jonas.deipenbrock@gmx.de>
2022-04-24 16:00:17 +0200

    starts building microphone logic
    Signed-off-by: Jonas Deipenbrock <jonas.deipenbrock@gmx.de>

--- main.js
+++ main.js
@@ -1,6 +1,43 @@
-import './style.css'
+import "./style.css";
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+// init recognition
+const speechRecognition = SpeechRecognition || webkitSpeechRecognition;
+const recognition = new speechRecognition();
+recognition.continuous = false;
+recognition.interimResults = false;
+recognition.lang = "en-US";
+
+recognition.onresult = function (e) {
+    document.getElementById("transcript").value = e.results[0][0].transcript;
+    recognition.stop();
+    // document.getElementById('labnol').submit();
+};
+
+recognition.onerror = function (e) {
+    recognition.stop();
+};
+
+const btn = document.querySelector("#btn");
+btn.addEventListener("click", () => {
+    console.log("click");
+    btn.innerHTML = "clicked";
+});
+
+const microphoneButton = document.querySelector("#microphone-img");
+microphoneButton.addEventListener("click", startDictation());
+
+const startDictation = () => {
+    console.log("listening");
+
+    microphoneButton.removeEventListener("click", startDictation);
+    microphoneButton.addEventListener("click", stopDictation);
+
+    recognition.start();
+};
+
+const stopDictation = () => {
+    console.log("stop listening");
+
+    microphoneButton.removeEventListener("click", stopDictation);
+    microphoneButton.addEventListener("click", startDictation);
+};
